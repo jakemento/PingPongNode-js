0d1ec4b1af5e1f1c5ecd9ac95e5d48f85b55ef94
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-13 14:02:23 -0700

    build and clean asks added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var buildProduction = utilities.env.production;
+
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -17,8 +21,20 @@ gulp.task('jsBrowserify', ['concatInterface'] , function() {
     .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task("minifyScripts", ["jsBrowserify"], function(){
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
     .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function() {
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
